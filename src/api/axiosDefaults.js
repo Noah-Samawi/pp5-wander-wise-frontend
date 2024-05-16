6ac27a703257dcf81e36de95b167f0b9bc38b1b7
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-05-16 11:04:51 +0000

    Added function to the code to make it possible for users to like and unlike posts

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://pp5-apis-e3b849e62ff3.herokuapp.com/";
+
+axios.defaults.baseURL = "https://pp5-wander-wise-frontend-63919ac97d38.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
