4b2ba8f91f29a30a9d49c4da2caa21314dc2bb56
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-05-19 13:33:43 +0000

    Added created the ProfilePage header.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 
-//axios.defaults.baseURL = "https://pp5-wander-wise-frontend-63919ac97d38.herokuapp.com/";
-axios.defaults.baseURL = "https://8000-noahsamawi-pp5apis-dcnxiuc6e2p.ws-eu114.gitpod.io";
+axios.defaults.baseURL = "https://pp5-wander-wise-frontend-63919ac97d38.herokuapp.com/";
+//axios.defaults.baseURL = "https://8000-noahsamawi-pp5apis-dcnxiuc6e2p.ws-eu114.gitpod.io";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+export const axiosRes = axios.create();/workspace/pp5-wander-wise-frontend/src/styles
