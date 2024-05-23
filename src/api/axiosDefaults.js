bffeebe80bde93d619a739a4baa7d133856a51f4
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-05-23 11:14:20 +0000

    deployment commit

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-axios.defaults.baseURL = "https://pp5-wander-wise-frontend-63919ac97d38.herokuapp.com/";
-//axios.defaults.baseURL = "https://8000-noahsamawi-pp5apis-dcnxiuc6e2p.ws-eu114.gitpod.io";
+axios.defaults.baseURL = "https://pp5-apis-e3b849e62ff3.herokuapp.com/";
+//axios.defaults.baseURL = "https://8000-noahsamawi-pp5apis-5r1sq2rva56.ws-eu114.gitpod.io";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
