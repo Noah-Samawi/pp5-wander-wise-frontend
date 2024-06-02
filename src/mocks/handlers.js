b74d9fd193f740fbe25b4a1413374f13f310745b
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-06-02 06:39:13 +0000

    correct the the api url in handlers.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,30 +1,26 @@
 import { rest } from "msw";
 
-const baseURL = "https://pp5-wander-wise-frontend-63919ac97d38.herokuapp.com/";
+const baseURL = "https://pp5-apis-e3b849e62ff3.herokuapp.com/";
 
-// Mock API handlers for user authentication-related requests
 export const handlers = [
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
     return res(
       ctx.json({
-        // "id": 3,
-        // "owner": "noah",
-        // "created_at": "22 May 2024",
-        // "updated_at": "22 May 2024",
-        // "name": "",
-        // "content": "",
-        // "image": "https://res.cloudinary.com/djebesftb/image/upload/v1/media/../default_profile_vm4gvx",
-
+        id: 3,
+        owner: "Noah-Samawi",
+        created_at: "23 May 2024",
+        updated_at: "02 Jun 2024",
+        name: "",
+        content: "",
+        image: "https://res.cloudinary.com/djebesftb/image/upload/v1/media/images/tinywow_tinywow_pexels-kelvin809-810775_57305395_57305437_fydzve",
       })
     );
   }),
 
-  // Mock POST request for logging out
   rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 
-  // Mock POST request for refreshing the authentication token
   rest.post(`${baseURL}dj-rest-auth/token/refresh/`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
